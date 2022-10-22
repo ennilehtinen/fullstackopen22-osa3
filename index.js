c448ca4e96affdbd349037a2093c1aef6b1082f7
ennilehtinen <ennilehtinen9@gmail.com>
2022-10-22 19:46:33 +0300

    osa-3.21

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
-const { response } = require('express')
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
 morgan.token('body', function (req, res) {
   return JSON.stringify(req.body)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 const app = express()
 
@@ -31,24 +47,6 @@ app.use(
   })
 )
 
-let persons = [
-  {
-    id: 1,
-    name: 'Arto Hellas',
-    number: '040-1234567'
-  },
-  {
-    id: 2,
-    name: 'Ada Lovelace',
-    number: '040-1234568'
-  },
-  {
-    id: 3,
-    name: 'Dan Abramov',
-    number: '040-1234569'
-  }
-]
-
 app.get('/info', (req, res) => {
   res.send(
     `<p>Phonebook has info for ${persons.length} people</p><p>${new Date()}</p>`
@@ -56,11 +54,14 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(result => {
+    res.json(result)
+  })
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
   const newPerson = req.body
+
   if (!newPerson.name) {
     return res.status(400).json({
       error: 'name missing'
@@ -71,36 +72,60 @@ app.post('/api/persons', (req, res) => {
       error: 'number missing'
     })
   }
-  if (persons.find(person => person.name === newPerson.name)) {
+  /*  if (persons.find(person => person.name === newPerson.name)) {
     return res.status(400).json({
       error: 'duplicate contact'
     })
-  }
-  newPerson.id = Math.ceil(Math.random() * 1000000)
-  persons.push(newPerson)
-  res.json(newPerson)
+  } */
+  const person = new Person(newPerson)
+
+  person
+    .save()
+    .then(result => {
+      console.log(`added ${result.name} number ${result.number} to phonebook`)
+      res.json(result)
+    })
+    .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  const person = {
+    name,
+    number
   }
+
+  Person.findByIdAndUpdate(request.params.id, person, {
+    new: true,
+    runValidators: true,
+    context: 'query'
+  })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).end()
-  } else {
-    response.status(404).end()
-  }
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
+
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(result => {
+      response.json(result)
+    })
+    .catch(error => next(error))
+})
+
+app.use(unknownEndpoint)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
